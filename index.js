5aa4b900fd22d03171f80739dd94fada04aa8f11
Alexis León <alexis.leons@pucp.pe>
2021-11-14 22:03:06 +0000

    First commit

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+//test
 //client.on('guildMemberAdd', member => {
 //  const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
 //  if (!channel) return;
